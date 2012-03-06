53078f3b28d5eff3a0997da68de8756905f34bde
Danesh M <daneshm90@gmail.com>
2012-03-05 21:38:27 -0500

    Settings : NavigationBar Customization
    Settings side to customize the navigation bar buttons.
    
    Patchset 1  : Initial commit
    Patchset 2  : Debug cleanup
    Patchset 3  : Hide navbar option on tablets
    Patchset 4  : Make language more user friendly and clean up/remove unused and unnecessary strings
    Patchset 5  : Ignore this, somehow uploaded the wrong commit
    Patchset 6  : Actual cleanup and remembering to escape special characters
    Patchset 7  : Whitespace cleanup
                  Only save layout if toggle switch is used
    Patchset 8  : Hide navbar option on phones without onscreen controls
    Patchset 9  : New layout for settings
                : Unlock icons by blunden
    Patchset 10 : Switch to global color resource
                : Label padding
    Patchset 11 : Fix padding and layout
    
    Change-Id: I39d9362f1930dc4181d20ff95077b4c290f6907e
    
    Conflicts:
    
    	res/xml/system_settings.xml
    	src/com/android/settings/cyanogenmod/SystemSettings.java

--- src/com/android/settings/cyanogenmod/SystemSettings.java
+++ src/com/android/settings/cyanogenmod/SystemSettings.java
@@ -36,6 +36,7 @@ public class SystemSettings extends SettingsPreferenceFragment implements
 
     private static final String KEY_FONT_SIZE = "font_size";
     private static final String KEY_COMBINED_BAR = "combined_bar";
+    private static final String KEY_NAVIGATION_BAR = "navigation_bar";
 
     private ListPreference mFontSizePref;
 
@@ -51,6 +52,10 @@ public class SystemSettings extends SettingsPreferenceFragment implements
         mFontSizePref.setOnPreferenceChangeListener(this);
         if (!Utils.isScreenLarge()) {
             getPreferenceScreen().removePreference(findPreference(KEY_COMBINED_BAR));
+        }
+        if (Utils.isScreenLarge() || !getResources().getBoolean(
+                com.android.internal.R.bool.config_showNavigationBar)) {
+            getPreferenceScreen().removePreference(findPreference(KEY_NAVIGATION_BAR));
         }
     }
 
