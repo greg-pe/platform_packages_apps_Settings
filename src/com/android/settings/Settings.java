56e1e12cec5d9d5f92f419b9d3583da9e780eded
Rowan Decker <smasher816@gmail.com>
2012-09-08 06:28:56 +0200

    Change Profiles to use the default header
    Personally it seems strange to have only one toggle so low on the page.
    In addition, unlike wifi most people will leave profiles enable or disabled.
    Therefore, it makes more sense to have only a toggle on the page itself as with "Developer options".
    
    Before:
    http://androtransfer.com/development/smasher816/media/Screenshot_2012-09-07-23-45-08.png
    http://androtransfer.com/development/smasher816/media/Screenshot_2012-09-07-23-45-36.png
    
    After:
    http://androtransfer.com/development/smasher816/media/Screenshot_2012-09-08-00-00-02.png
    http://androtransfer.com/development/smasher816/media/Screenshot_2012-09-07-23-45-36.png
    
    Change-Id: Ifde1b7d414abbb189b502ddbe8e14b2ae38a0513

--- src/com/android/settings/Settings.java
+++ src/com/android/settings/Settings.java
@@ -577,8 +577,7 @@ public class Settings extends PreferenceActivity
             if (header.fragment == null && header.intent == null) {
                 return HEADER_TYPE_CATEGORY;
             } else if (header.id == R.id.wifi_settings
-                    || header.id == R.id.bluetooth_settings
-                    || header.id == R.id.profiles_settings) {
+                    || header.id == R.id.bluetooth_settings) {
                 return HEADER_TYPE_SWITCH;
             } else {
                 return HEADER_TYPE_NORMAL;
