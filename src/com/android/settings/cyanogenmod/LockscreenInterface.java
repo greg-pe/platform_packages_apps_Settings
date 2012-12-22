56e9d241266061363e66152db673eb33a939b1b6
Greg_Maahs <projecteliteroms@gmail.com>
2012-12-22 09:32:25 -0500

    Fix Lock screen Battery status summary display credit https://github.com/CyanogenMod/android_packages_apps_Settings/commit/41475909c60910a84c153e401a94f39ccc0b84aa

--- src/com/android/settings/cyanogenmod/LockscreenInterface.java
+++ src/com/android/settings/cyanogenmod/LockscreenInterface.java
@@ -24,8 +24,8 @@ import android.provider.Settings;
 import com.android.settings.R;
 import com.android.settings.SettingsPreferenceFragment;
 
-public class LockscreenInterface extends SettingsPreferenceFragment implements Preference.OnPreferenceChangeListener {
-    private static final String TAG = "LockscreenInterface";
+public class LockscreenInterface extends SettingsPreferenceFragment implements
+    Preference.OnPreferenceChangeListener {
 
     private static final String KEY_ALWAYS_BATTERY_PREF = "lockscreen_battery_status";
 
@@ -42,25 +42,13 @@ public class LockscreenInterface extends SettingsPreferenceFragment implements P
         addPreferencesFromResource(R.xml.lockscreen_interface_settings);
 
         mBatteryStatus = (ListPreference) findPreference(KEY_ALWAYS_BATTERY_PREF);
-        mBatteryStatus.setOnPreferenceChangeListener(this);
-
-    }
-
-    @Override
-    public void onResume() {
-        super.onResume();
-    }
-
-    @Override
-    public void onPause() {
-        super.onPause();
-
-        // Set the battery status description text
+   
         if (mBatteryStatus != null) {
             int batteryStatus = Settings.System.getInt(getActivity().getApplicationContext().getContentResolver(),
                     Settings.System.LOCKSCREEN_ALWAYS_SHOW_BATTERY, 0);
             mBatteryStatus.setValueIndex(batteryStatus);
             mBatteryStatus.setSummary(mBatteryStatus.getEntries()[batteryStatus]);
+            mBatteryStatus.setOnPreferenceChangeListener(this);
         }
     }
 
